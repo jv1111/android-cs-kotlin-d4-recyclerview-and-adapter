b06e7dabcd3429dcc95fb5308be1273b6792da98
jv1111 <webd0829@gmail.com>
2023-06-30 17:07:33 +0800

    added Lod.d

--- app/src/main/java/com/example/d4_kotlin_recyclerview/Adapter.kt
+++ app/src/main/java/com/example/d4_kotlin_recyclerview/Adapter.kt
@@ -1,5 +1,6 @@
 package com.example.d4_kotlin_recyclerview
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,16 +12,19 @@ class Adapter(
     private val clickListener:(Fruit)->Unit //unit is equivalent to void
 ) : RecyclerView.Adapter<ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        Log.i("myTag", "onCreateViewHolder")
         val layoutInflater = LayoutInflater.from(parent.context)
         val listItem = layoutInflater.inflate(R.layout.list_item, parent, false)
         return ViewHolder(listItem)
     }
 
     override fun getItemCount(): Int {
+        Log.i("myTag", "setting the size")
         return fruitsList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i("myTag", "onBind function")
         val fruit = fruitsList[position];
 //        holder.myTextView.text = fruit.name
         holder.bind(fruit, clickListener)
@@ -31,6 +35,7 @@ class Adapter(
 class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 //    val myTextView = view.findViewById<TextView>(R.id.tvName)
     fun bind(fruit: Fruit, clickListener: (Fruit) -> Unit){
+        Log.i("myTag", "binding function")
         val myTextView = view.findViewById<TextView>(R.id.tvName)
         myTextView.text = fruit.name
     //    add listener
--- app/src/main/java/com/example/d4_kotlin_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/d4_kotlin_recyclerview/MainActivity.kt
@@ -3,26 +3,31 @@ package com.example.d4_kotlin_recyclerview
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     val fruitsList = listOf<Fruit>(
         Fruit("F1", "S1"),
         Fruit("F2", "S2"),
-        Fruit("F3", "S3"),
-        Fruit("F4", "S4"),
-        Fruit("F5", "S5"),
-        Fruit("F6", "S6"),
-        Fruit("F7", "S7"),
+//        Fruit("F3", "S3"),
+//        Fruit("F4", "S4"),
+//        Fruit("F5", "S5"),
+//        Fruit("F6", "S6"),
+//        Fruit("F7", "S7"),
     )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.setBackgroundColor(Color.YELLOW)
+
         recyclerView.layoutManager = LinearLayoutManager(this)//setting layout manager
+        Log.i("myTag", "onCreate")
         recyclerView.adapter = Adapter(
             fruitsList
         ) { selectedItem: Fruit ->
@@ -30,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun listItemClicked(fruit: Fruit){
+        Log.d("myTag", "Making a toast message")
         Toast.makeText(this@MainActivity, "Supplier: ${fruit.supplier}", Toast.LENGTH_LONG).show()
     }
 }
